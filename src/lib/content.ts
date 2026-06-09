193b1dbf740f774eaf299bc2f50b107654026284
anguishe <anguisheh1@gmail.com>
2026-06-09 17:45:33 -0500

    Kais Run Optimization Update 3.6

--- src/lib/content.ts
+++ src/lib/content.ts
@@ -66,7 +66,7 @@ export const LICENSE_DISPLAY = {
 } as const
 
 export const SOCIAL_LINKS = {
-  facebook: 'https://www.facebook.com/profile.php?id=61578080548022',
+  facebook: 'https://www.facebook.com/beachhousemovingfl/',
   facebookHandle: '@beachhousemovingfl',
   google: 'https://g.page/r/CXl8yvSwTlBcEAI',
   yelp: '', // Add Yelp URL when verification is complete
